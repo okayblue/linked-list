2b22144ac43ca81986d9c75fabf4d93eba271a2e
okayblue <56899411+okayblue@users.noreply.github.com>
2022-09-21 17:02:45 -0400

    Add prepend method

--- main.rb
+++ main.rb
@@ -5,25 +5,30 @@ class LinkedList
   end
 
   def append(value)
-    if @head == nil
+    if @head == nil 
       @head = Node.new(value)
     else
       tail.next_node = Node.new(value)
     end
   end
+
   def tail
     node = @head
     return node if !node.next_node
-    return node if !node.next_node while (node = node.next)
+    return node if !node.next_node while (node = node.next_node)
+  end
+
+  def prepend(value)
+    @head = Node.new(value, @head)
   end
 end
 
 class Node
   attr_accessor :value, :next_node
 
-  def initialize(value)
+  def initialize(value = nil, next_node = nil)
     @value = value
-    @next_node = nil
+    @next_node = next_node
   end
 end
 
@@ -32,4 +37,7 @@ p list
 list.append(14)
 p list
 list.append(33)
+list.append(33433)
+p list
+list.prepend(444)
 p list
